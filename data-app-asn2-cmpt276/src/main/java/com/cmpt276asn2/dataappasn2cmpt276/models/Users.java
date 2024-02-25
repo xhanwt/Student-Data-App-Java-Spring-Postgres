2d61b292df57cd12af38f1c6a82aaf58903884ec
Han <hanwinhtun334@gmail.com>
2024-02-25 12:50:06 -0800

    fix

---
+++ data-app-asn2-cmpt276/src/main/java/com/cmpt276asn2/dataappasn2cmpt276/models/Users.java
@@ -0,0 +1,35 @@
+package com.cmpt276asn2.dataappasn2cmpt276.models;
+
+public class Users {
+    private String name;
+    private String password;
+    private int size;
+
+    public Users() {
+    }
+    public Users(String name, String password, int size) {
+        this.name = name;
+        this.password = password;
+        this.size = size;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    
+}
